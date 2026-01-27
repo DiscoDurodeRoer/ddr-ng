967c08c2c478d7e48f86b617056531f3bd272698
discoduroderoer <administrador@discoduroderoer.es>
2026-01-27 20:40:40 +0100

    remove cache

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,17 +8,23 @@ import { DdrAction, DdrSelectItem, DdrTreeNode } from 'ddr-ng';
 export class DataService {
 
   async getChangelog() {
-    const changelog = await fetch(`${environment.urlData}/changelog.json`).then(async (value: Response) => await value.json() as DdrTreeNode<string>[])
+    const changelog = await fetch(`${environment.urlData}/changelog.json`, {
+      cache: 'no-store'
+    }).then(async (value: Response) => await value.json() as DdrTreeNode<string>[])
     return changelog.sort((a: DdrTreeNode<string>, b: DdrTreeNode<string>) => b.name.localeCompare(a.name))
   }
 
   async getVersions() {
-    const versions = await fetch(`${environment.urlData}/versions.json`).then(async (value: Response) => await value.json() as DdrAction<string>[] | DdrSelectItem<string>[])
+    const versions = await fetch(`${environment.urlData}/versions.json`, {
+      cache: 'no-store'
+    }).then(async (value: Response) => await value.json() as DdrAction<string>[] | DdrSelectItem<string>[])
     return versions.sort((a, b) => b.label.localeCompare(a.label))
   }
 
   async getLanguages() {
-    const languages = await fetch(`${environment.urlData}/languages.json`).then(async (value: Response) => await value.json() as DdrAction<string>[])
+    const languages = await fetch(`${environment.urlData}/languages.json`, {
+      cache: 'no-store'
+    }).then(async (value: Response) => await value.json() as DdrAction<string>[])
     return languages.map((language) => ({
       ...language,
       img: `${environment.urlData}${language.img}`
